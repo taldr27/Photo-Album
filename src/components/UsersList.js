0560c31079368d640d8147d54177b8727c1e278c
taldr27 <diegogarciaq98@gmail.com>
2023-02-28 14:31:00 -0500

    Add User slice, thunk and the component

---
+++ src/components/UsersList.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
+function UsersList() {
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users; // { data: [], isLoading: false, error: null }
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error fetching data</div>
+  }
+
+  return (
+    <div>
+      {data.length}
+    </div>
+  );
+}
+
+export default UsersList;
---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // Update our state object however appropiatae
+      // to show the user what we are loading data
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
+});
+
+export const usersReducer = usersSlice.reducer;
